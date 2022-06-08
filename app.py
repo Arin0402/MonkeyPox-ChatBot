74f92a8387e56e96c84bed5bfcc9e0b70986bf72
Arin0402 <goar045@gmail.com>
2022-06-09 01:10:07 +0530

    first commit 0912

---
+++ app.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+   app.run()
